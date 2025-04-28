7ff2683219ea159fcfd32ef66d6e9970c1eb75ba
Ma7en <m9ee9m@gmail.com>
2025-04-28 23:21:19 +0300

    edit page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,18 @@
-'use strict';
-
-
+"use strict";
 
 /**
  * add event on element
  */
 
 const addEventOnElem = function (elem, type, callback) {
-  if (elem.length > 1) {
-    for (let i = 0; i < elem.length; i++) {
-      elem[i].addEventListener(type, callback);
+    if (elem.length > 1) {
+        for (let i = 0; i < elem.length; i++) {
+            elem[i].addEventListener(type, callback);
+        }
+    } else {
+        elem.addEventListener(type, callback);
     }
-  } else {
-    elem.addEventListener(type, callback);
-  }
-}
-
-
+};
 
 /**
  * navbar toggle
@@ -34,8 +30,6 @@ const closeNavbar = () => navbar.classList.remove("active");
 
 addEventOnElem(navLinks, "click", closeNavbar);
 
-
-
 /**
  * header & back top btn active when scroll down to 100px
  */
@@ -44,18 +38,16 @@ const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 const headerActive = function () {
-  if (window.scrollY > 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
-  }
-}
+    if (window.scrollY > 100) {
+        header.classList.add("active");
+        backTopBtn.classList.add("active");
+    } else {
+        header.classList.remove("active");
+        backTopBtn.classList.remove("active");
+    }
+};
 
 addEventOnElem(window, "scroll", headerActive);
-
-
 
 /**
  * filter function
@@ -67,24 +59,22 @@ const filterItems = document.querySelectorAll("[data-filter]");
 let lastClickedFilterBtn = filterBtns[0];
 
 const filter = function () {
-  lastClickedFilterBtn.classList.remove("active");
-  this.classList.add("active");
-  lastClickedFilterBtn = this;
+    lastClickedFilterBtn.classList.remove("active");
+    this.classList.add("active");
+    lastClickedFilterBtn = this;
 
-  for (let i = 0; i < filterItems.length; i++) {
-    if (this.dataset.filterBtn === filterItems[i].dataset.filter ||
-      this.dataset.filterBtn === "all") {
-
-      filterItems[i].style.display = "block";
-      filterItems[i].classList.add("active");
-
-    } else {
-
-      filterItems[i].style.display = "none";
-      filterItems[i].classList.remove("active");
-
+    for (let i = 0; i < filterItems.length; i++) {
+        if (
+            this.dataset.filterBtn === filterItems[i].dataset.filter ||
+            this.dataset.filterBtn === "all"
+        ) {
+            filterItems[i].style.display = "block";
+            filterItems[i].classList.add("active");
+        } else {
+            filterItems[i].style.display = "none";
+            filterItems[i].classList.remove("active");
+        }
     }
-  }
-}
+};
 
 addEventOnElem(filterBtns, "click", filter);
